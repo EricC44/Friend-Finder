3b9c7bbcb333a27022c089598504c011ae13efa0
ECarlson <ericcarlson4@outlook.com>
2019-06-02 22:40:43 -0700

    have an example to test when the time comes

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendsList = [
+
+
+{
+    name: "Casey",
+    photo: "http://zapatasbatonrouge.com/wp-content/uploads/parser/cute-medium-short-haircuts-for-women-1.jpg",
+    scores: [5, 4, 1, 3, 4, 1, 1, 1, ]
+}
+
+]
