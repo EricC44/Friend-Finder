1c4f217a386a06e2b096967a010fad1b629cbb56
ECarlson <ericcarlson4@outlook.com>
2019-06-02 23:40:47 -0700

    small adjustments to the friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,7 @@ var friendsList = [
 {
     name: "Casey",
     photo: "http://zapatasbatonrouge.com/wp-content/uploads/parser/cute-medium-short-haircuts-for-women-1.jpg",
-    scores: [5, 4, 1, 3, 4, 1, 1, 1, ]
+    scores: [5, 4, 1, 3, 4, 1, 1, 1, 4, 2]
 }
 
 ]
