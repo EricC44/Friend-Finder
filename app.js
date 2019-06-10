ee25c3f29f54d36c3d1b9424d4f71637808972a5
ECarlson <ericcarlson4@outlook.com>
2019-06-09 23:56:40 -0700

    making it look pretty

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-//Required Dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require ('path');
-//Configure express
-var app = express();
-var PORT = process.env.PORT || 8080;
+// Required Dependencies
+var express = require('express')
+var bodyParser = require('body-parser')
+var path = require ('path')
+// Configure express
+var app = express()
+var PORT = process.env.PORT || 8080
 
-//Add the middleware for parsing incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
+// Add the middleware for parsing incoming requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
 
-//Make sure express has access to the css files
-app.use(express.static(__dirname + '/app/css'));
+// Make sure express has access to the css files
+// app.use(express.static(__dirname + '/app/css'));
 
-//Add the routes using the path package
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+// Add the routes using the path package
+require(path.join(__dirname, './app/routing/apiRoutes'))(app)
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app)
 
-//Activate the PORT to begin
-app.listen(PORT, function() {
-console.log(`Hello? You are connected on PORT ${PORT}`);
-});
+// Activate the PORT to begin
+app.listen(PORT, function () {
+console.log(`Hello? You are connected on PORT ${PORT}`)
+})
